8716727bc7ee45327bad31434f0c36a20d3d5ed8
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2020-12-08 02:25:26 -0800

    styling

--- src/Quiz.js
+++ src/Quiz.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { QuizQuestions } from './QuizQuestions'
 import './styles.css'
 
+
 export class Quiz extends Component {
 
   constructor(props) { //Quiz component where we store the states needed
@@ -95,26 +96,30 @@ export class Quiz extends Component {
     }
     return (
       <div>
-        <h2>{question}</h2>
-        <span>{`Question ${currentIndex + 1} of ${QuizQuestions.length}`}</span>
-        {
-          options.map(option =>
-          <p key = {option.id} className={`options ${userAnswer === option? "selected": null}`} 
-          onClick = {() => this.checkAnswer(option)}
-          >
-            {option}
-          </p>  
-          )
-        }
+        <div class="container">
+          <div class="inner">
+            <h2>{question}</h2>
+            {
+              options.map(option =>
+              <p key = {option.id} className={`options ${userAnswer === option? "selected": null}`} 
+              onClick = {() => this.checkAnswer(option)}
+              >
+                {option}
+              </p>  
+              )
+            }
 
-        {currentIndex < QuizQuestions.length - 1 && 
-        <button disabled = {this.state.disabled} onClick={this.nextQuestionHandler}>
-          Next
-        </button>}
-        {currentIndex === QuizQuestions.length-1 && 
-        <button disabled= {this.state.disabled} onClick={this.finishHandler}>
-          All Done!
-        </button>}
+            {currentIndex < QuizQuestions.length - 1 && 
+            <button class= "next" disabled = {this.state.disabled} onClick={this.nextQuestionHandler}>
+            Next
+            </button>}
+            {currentIndex === QuizQuestions.length-1 && 
+            <button class= "next" disabled= {this.state.disabled} onClick={this.finishHandler}>
+              All Done!
+            </button>}
+          </div>
+        </div>
+        <span>{`Question ${currentIndex + 1} of ${QuizQuestions.length}`}</span>
       </div>
     )
   }
